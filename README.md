b218abe5212d2a722968ce8d8a9da809655d3c35
saroj-rumsan <saroj.shrestha@rumsan.net>
2024-04-03 13:06:06 +0545

    Revert "Stash Demo"
    This reverts commit 82bfc623d1d186ed654a100f25ec0612dc669a12.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # NestJs Microservice
-
-This is a sample NestJS microservice.
-
-This microservice is built using NestJS framework and is designed to handle specific functionality.
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,14 +16,12 @@ export class AppController {
       0,
     );
     throw new RpcException('This is a custom error message');
-
     return sum;
   }
 
   @EventPattern('user_created')
   createUser(nums: number[]): Array<number> {
     const multipliedNumbers = nums.map((number) => number * 73);
-    console.log('Hello World!');
     return multipliedNumbers;
   }
 }
